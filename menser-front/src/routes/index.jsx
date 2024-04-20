c2ad0bd135b36bebd712ef0b3fadd1dc5c3f7af6
gustavolaradossantos <gustavolaradossantos@gmail.com>
2024-04-20 12:41:15 -0300

    feat: implement react router

---
+++ menser-front/src/routes/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import Home from "../screens/Home";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+
+
+function AppRoutes() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/signIn',
+      element: <SignIn />,
+    },
+    {
+      path: '/signUp',
+      element: <SignUp />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" replace />,
+    }
+  ]);
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default AppRoutes;
